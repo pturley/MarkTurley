5e005f21194d4e7bf35931228c66b08193c645a9
Patrick Turley [pturley] <patrick.turley@gmail.com>
2014-03-16 15:42:17 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MarkTurley::Application.config.secret_key_base = '4601fda4ed80a53189de2d5dd99970ef72f963534d05d3032abc5718f2678c4a73f466124c8baf3b1f5a0f68baa7b11bf62d89ee7be779fcd9d4db04e3dbab6f'
